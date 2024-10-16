2801c2130ddc6dcf5cd279dc8dbece7ef868193e
cjremmett <cjremmett@gmail.com>
2024-10-16 08:32:40 -0400

    some work on ioffice component

--- src/app/ioffice/ioffice.component.ts
+++ src/app/ioffice/ioffice.component.ts
@@ -9,37 +9,30 @@ import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 })
 export class IofficeComponent
 {
-  //Need to use angular template instead
-  // @ViewChild('email_address') emailAddressElement: ElementRef;
+  secretKeyInputText = '';
+  emailAddressInputText = '';
+  async updateSettings(monday_checkin: boolean, tuesday_checkin: boolean, wednesday_checkin: boolean, thursday_checkin: boolean, friday_checkin: boolean): Promise<void>
+  {
+    const json_body = JSON.parse('{"email_address": "' + this.emailAddressInputText + '", "secret_key": "' + this.secretKeyInputText + '", "monday_checkin": "' + monday_checkin + '", "tuesday_checkin": "' + tuesday_checkin + '", "wednesday_checkin": "' + wednesday_checkin + '", "thursday_checkin": "' + thursday_checkin + '", "friday_checkin": "' + friday_checkin + '"}');
+    const response = await fetch('https://cjremmett.com/flask/gafg-tools/ioffice-checkin-user-update-settings', {
+      method: 'PUT',
+      headers: 
+      {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(json_body)
+    });
   
-  // ngAfterViewInit()
-  // {
-  //   console.log(this.emailAddressElement);
-  // }
-  
-}
-
-async function updateSettings(email_address: string, secret_key:string, monday_checkin: boolean, tuesday_checkin: boolean, wednesday_checkin: boolean, thursday_checkin: boolean, friday_checkin: boolean) {
-  const json_body = JSON.parse('{"email_address": "' + email_address + '", "secret_key": "' + secret_key + '", "monday_checkin": "' + monday_checkin + '", "tuesday_checkin": "' + tuesday_checkin + '", "wednesday_checkin": "' + wednesday_checkin + '", "thursday_checkin": "' + thursday_checkin + '", "friday_checkin": "' + friday_checkin + '"}');
-  const response = await fetch('https://cjremmett.com/flask/gafg-tools/ioffice-checkin-user-update-settings', {
-    method: 'PUT',
-    headers: 
+    const res_text = await response.text();
+    if(response.status === 200)
     {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(json_body)
-  });
-
-  const res_text = await response.text();
-  if(response.status === 200)
-  {
-    window.alert("Settings updated successfully!");
-    // document.getElementById('email_address').value = '';
-    // document.getElementById('secret_key').value = '';
-  }
-  else
-  {
-    window.alert("Settings update failed! Error message:\n\n" + res_text);
-  }
-}; 
-
+      window.alert("Settings updated successfully!");
+      this.secretKeyInputText = '';
+      this.emailAddressInputText = '';
+    }
+    else
+    {
+      window.alert("Settings update failed! Error message:\n\n" + res_text);
+    }
+  };
+}
