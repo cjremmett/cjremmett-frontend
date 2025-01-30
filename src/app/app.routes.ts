4065aac1e0a1b4f4f3aad33090d1067aee1ac94f
cjremmett <cjremmett@gmail.com>
2025-01-30 14:28:19 -0500

    added placeholder for photography app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { IconGridComponent } from './icon-grid/icon-grid.component';
 import { IofficeComponent } from './ioffice/ioffice.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PhotographyComponent } from './photography/photography.component';
 
 export const routes: Routes = [
     { path: '', component: IconGridComponent },
     { path: 'ioffice', component: IofficeComponent },
+    { path: 'photography', component: PhotographyComponent },
     { path: '**', component: PageNotFoundComponent}
   ];
