f8cbc342418be4995f04817ca51b1a64b03e1a55
cjremmett <cjremmett@gmail.com>
2025-02-05 09:35:26 -0500

    photo frontend

--- src/app/photography/photography.component.ts
+++ src/app/photography/photography.component.ts
@@ -16,23 +16,6 @@ export class PhotographyComponent implements OnInit
   constructor(private activatedRoute: ActivatedRoute, private photosService: PhotosService) {
   }
 
-  // photos = [];
-  //ngOnInit(): void
-  //{
-    // this.activatedRoute.queryParams.subscribe(queryParams => {
-    //   console.log('Tags: ' + queryParams['tags']);
-    //   let query = '';
-    //   if(queryParams['tags'] != null && queryParams['tags'] !== '')
-    //   {
-    //     query = '?tags= ' + queryParams['tags'];
-    //   }
-      
-    //   fetch('https://cjremmett.com/api/photography/get-photos' + query, { method: 'GET' }).then(async function(this: PhotographyComponent, response) {
-    //     this.photos = await response.json();
-    //   });
-    // });
-  //}
-
   photos: Photo[] = [];
 
   ngOnInit(): void
@@ -49,26 +32,4 @@ export class PhotographyComponent implements OnInit
       });
     });
   }
-
-  // photos = [
-  //   {
-  //     "_id": "679b9ce5d462b5cf45c325c3",
-  //     "id": "34075325-8a92-4778-8515-7eebde95450f",
-  //     "big_thumb": "big_thumb.png",
-  //     "camera": "SONY - ILCE-7M2",
-  //     "fNumber": "f/6.3",
-  //     "focalLength": "600 mm",
-  //     "full": "full.png",
-  //     "iso": "400",
-  //     "lens": "FE 200-600mm F5.6-6.3 G OSS",
-  //     "raw": "raw.ARW",
-  //     "shutterSpeed": "1/500",
-  //     "small_thumb": "small_thumb.png",
-  //     "tags": {
-  //       "wildlife": true,
-  //       "bird": true
-  //     },
-  //     "uploadTimestamp": "1737484270652"
-  //   }
-  // ]
 }
