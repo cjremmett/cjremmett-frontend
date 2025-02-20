3d50a8d03e64d314cd143fd36a9b4c3497086af6
cjremmett <cjremmett@gmail.com>
2025-02-20 16:59:15 -0500

    photo frontend

--- src/app/photo-upload/photo-upload.component.ts
+++ src/app/photo-upload/photo-upload.component.ts
@@ -1,22 +1,53 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-photo-upload',
   standalone: true,
-  imports: [],
+  imports: [ FormsModule ],
   templateUrl: './photo-upload.component.html',
   styleUrl: './photo-upload.component.css'
 })
 export class PhotoUploadComponent {
-  tags: string = '';
+  tags = '';
+  uuid = '';
   async createPhoto(): Promise<void>
   {
-    let tagsArray = this.tags.split(',')
+    console.log(this.tags);
+    let tagsArray = this.tags.split(',');
     let tagsJsonString = '{';
     for(const tag of tagsArray)
     {
+      console.log(tag);
       let tagTemp = (tag.trim()).toLowerCase();
-      tagsJsonString = tagsJsonString + '"' + tagTemp + '": true,'
+      console.log(tagTemp);
+      tagsJsonString = tagsJsonString + '"' + tagTemp + '": true,';
+    }
+    if(tagsJsonString.slice(-1) === ',')
+    {
+      tagsJsonString = tagsJsonString.slice(0, -1);
+    }
+    tagsJsonString = tagsJsonString + '}';
+    this.tags = tagsJsonString;
+
+    const response = await fetch('https://cjremmett.com/api/photography/create-photo', {
+      method: 'POST',
+      headers: 
+      {
+        "Content-Type": "application/json",
+      },
+      body: tagsJsonString
+    });
+  
+    let response_json = await response.json();
+    if(response.status < 300)
+    {
+      window.alert("Created new photo with ID: " + response_json['uuid']);
+      this.uuid = response_json['uuid'];
+    }
+    else
+    {
+      window.alert("Photo creation failed. Error message:\n\n" + JSON.stringify(response_json));
     }
   }
 }
