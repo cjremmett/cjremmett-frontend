4ae0750b3de9daa17d7dd5664db93d4d8e5a0356
cjremmett <cjremmett@gmail.com>
2025-02-20 07:47:54 -0500

    photo frontend

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { IofficeComponent } from './ioffice/ioffice.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { PhotographyComponent } from './photography/photography.component';
 import { PhotoDisplayComponent } from './photo-display/photo-display.component';
+import { PhotoUploadComponent } from './photo-upload/photo-upload.component';
 
 
 export const routes: Routes = [
     { path: '', component: IconGridComponent },
     { path: 'ioffice', component: IofficeComponent },
+    { path: 'photography/upload', component: PhotoUploadComponent },
     { path: 'photography/:id', component: PhotoDisplayComponent },
     { path: 'photography', component: PhotographyComponent },
     { path: '**', component: PageNotFoundComponent}
---
+++ src/app/photo-upload/photo-upload.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-upload',
+  standalone: true,
+  imports: [],
+  templateUrl: './photo-upload.component.html',
+  styleUrl: './photo-upload.component.css'
+})
+export class PhotoUploadComponent {
+  tags: string = '';
+  async createPhoto(): Promise<void>
+  {
+    let tagsArray = this.tags.split(',')
+    let tagsJsonString = '{';
+    for(const tag of tagsArray)
+    {
+      let tagTemp = (tag.trim()).toLowerCase();
+    }
+  }
+}
